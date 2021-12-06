6705c2a936457f64f61a3432d6e7b2f533ab9de1
TMarquet <72377479+TMarquet@users.noreply.github.com>
2021-12-06 15:34:13 +0100

    Update convert_to_tflite.py

--- source/convert_to_tflite.py
+++ source/convert_to_tflite.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 import os
 from tensorflow.keras.models import load_model
 # Convert the model
-from preprocessing import *
+from preprocessing import PATH_MODELS
 model = load_model(PATH_MODELS + 'model.h5')
 model.summary()
 converter = tf.lite.TFLiteConverter.from_keras_model(model) # path to the SavedModel directory
